2bc0d1900e8f23614ca4f796585ae37c9331e1ff
unknown <ayubfmohammed@gmail.com>
2024-10-04 22:08:05 -0600

    Update BSM.cpp

---
+++ Bruteforce/BruteforceStringMatch/bsm.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string.h>
+
+int main(){
+  std::string toMatch = "xo" ; 
+  std::string outerString = "hello this is xotext" ; 
+  for(int i =  0 ;)
+  std::cout << test << std::endl ; 
+  return 0 ; 
+}
